af9f116eaea5db4c2046931ce03d8f8a192c76c7
sebastian <sebastiansena347@gmail.com>
2026-06-04 16:19:13 -0500

    cambios en los t.s

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
-  imports: [
-    PrismaModule,
-    UsersModule,
-    AuthModule,
-  ],
+  imports: [PrismaModule, UsersModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
